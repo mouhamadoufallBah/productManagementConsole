9407fc5755ddf757de66b07c3b01bcce70f41047
mouhamadoufallbah <mouhamadoufall.bah@gmail.com>
2026-06-30 20:59:19 +0000

    feat: init categories table

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 <?php
+$categories = [
+    [
+        "code" => "c001",
+        "name" => "cat-1",
+        "products" => [
+            [
+                "ref" => "rprod-1",
+                "name" => "prod-1",
+                "quantity" => 100,
+                "price" => 500 
+            ],
+            [
+                "ref" => "rprod-2",
+                "name" => "prod-2",
+                "quantity" => 100,
+                "price" => 500 
+            ],
+        ]
+    ],
+    [
+        "code" => "c002",
+        "name" => "cat-2",
+        "products" => []
+    ]
+];
